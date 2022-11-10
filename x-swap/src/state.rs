662d42afa5807f35f74c397be176ca4e1f5f2738
Bogdan <bogdan.oloeriu@gmail.com>
2022-11-11 00:20:09 +0100

    added xswap notes comments

--- x-swap/src/state.rs
+++ x-swap/src/state.rs
@@ -7,6 +7,8 @@ pub struct Offer<M: ManagedTypeApi>  {
     pub from: ManagedAddress<M>,
     pub from_token: TokenIdentifier<M>,
     pub from_amount: BigUint<M>,
+    pub from_initial_amount: BigUint<M>,
+
     pub from_timestamp: u64,
 
     pub to_token: TokenIdentifier<M>,
