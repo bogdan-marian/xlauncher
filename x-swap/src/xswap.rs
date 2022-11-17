496f09cd2028dffcffb0e4a4a085710261ee37df
icegriffinguru <icegriffin1122@gmail.com>
2022-11-16 20:27:34 -0500

    [Ice] x-swap test 1 (ongoing)

--- x-swap/src/xswap.rs
+++ x-swap/src/xswap.rs
@@ -1,15 +1,14 @@
 #![no_std]
 
 elrond_wasm::imports!();
-elrond_wasm::derive_imports!();
 
-mod config;
-mod data;
-mod storage;
-mod event;
-mod user;
-mod admin;
-mod view;
+pub mod config;
+pub mod data;
+pub mod storage;
+pub mod event;
+pub mod user;
+pub mod admin;
+pub mod view;
 
 #[elrond_wasm::contract]
 pub trait XSwap:
