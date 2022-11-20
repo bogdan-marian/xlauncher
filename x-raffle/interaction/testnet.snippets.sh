624f2b66a31873952d498768b348973ca91dea7f
Bogdan <bogdan.oloeriu@gmail.com>
2022-11-20 11:39:26 +0100

    formated testnet

--- x-raffle/interaction/testnet.snippets.sh
+++ x-raffle/interaction/testnet.snippets.sh
@@ -9,7 +9,8 @@ LOGS_FOLDER="interaction-logs"
 TICKET_TOKEN="XRF-0a31bd"
 TICKET_TOKEN_HEX="0x$(echo -n ${TICKET_TOKEN} | xxd -p -u | tr -d '\n')"
 
-END_TIMESTAMP=1668826285
+# 01 december 2022
+END_TIMESTAMP=$(date -d '2022-12-01 21:00:00' +"%s")
 TICKET_PRICE=100000000000000000 # 0.1 ESDT
 NUMBER_OF_WINNERS=5
 PRIZE_PERCENTAGES="4000 2500 2000 1000 500"
@@ -17,41 +18,47 @@ PRIZE_PERCENTAGES="4000 2500 2000 1000 500"
 ###
 
 deploy() {
-    erdpy --verbose contract deploy  --project=${PROJECT} --recall-nonce --pem=${WALLET} --send --proxy=${PROXY} --chain=${CHAIN_ID} \
+  erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${WALLET} --send --proxy=${PROXY} --chain=${CHAIN_ID} \
     --outfile="${LOGS_FOLDER}/deploy-testnet.interaction.json" \
     --metadata-payable \
     --gas-limit=80000000
-    
-    ADDRESS=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['contractAddress']")
 
-    erdpy data store --key=address-devnet --value=${ADDRESS}
+  ADDRESS=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['contractAddress']")
 
-    echo ""
-    echo "Smart contract address: ${ADDRESS}"
+  erdpy data store --key=address-devnet --value=${ADDRESS}
+
+  echo ""
+  echo "Smart contract address: ${ADDRESS}"
 }
 
 upgrade() {
-    erdpy --verbose contract upgrade ${ADDRESS} --project=${PROJECT} --recall-nonce --pem=${WALLET} --send --outfile="upgrade.json" --proxy=${PROXY} --chain=${CHAIN_ID} \
+  erdpy --verbose contract upgrade ${ADDRESS} --project=${PROJECT} --recall-nonce --pem=${WALLET} --send --outfile="upgrade.json" --proxy=${PROXY} --chain=${CHAIN_ID} \
     --metadata-payable \
     --gas-limit=70000000
 }
 
 setTicketToken() {
-    erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
+  erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
     --gas-limit=8000000 \
     --function="setTicketToken" \
     --arguments ${TICKET_TOKEN_HEX}
 }
 
 openRound() {
-    erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
+  erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
     --gas-limit=8000000 \
     --function="openRound" \
     --arguments ${END_TIMESTAMP} ${TICKET_PRICE} ${NUMBER_OF_WINNERS} ${PRIZE_PERCENTAGES}
 }
 
 finishRound() {
-    erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
+  erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
+    --gas-limit=8000000 \
+    --function="finishRound"
+}
+
+buyTickets() {
+  erdpy --verbose contract call ${ADDRESS} --send --proxy=${PROXY} --chain=${CHAIN_ID} --recall-nonce --pem=${WALLET} \
     --gas-limit=8000000 \
     --function="finishRound"
 }
