8a7f766234b595d1a01f5fe4fbb9782510bfd681
Bogdan <bogdan.oloeriu@gmail.com>
2022-12-10 19:14:09 +0100

    config settings

--- x-swap-test/src/config.ts
+++ x-swap-test/src/config.ts
@@ -25,10 +25,10 @@ export const COMMON_GAS_LIMIT = 60_000_000;
 export const XSWAP_SC_ABI_URL = 'abi/x-swap.abi.json';
 export const XSWAP_SC_WASM_URL = 'abi/x-swap.wasm';
 export const XSWAP_SC_NAME = 'XSwap';
-export const XSWAP_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgqhjkd4353tn9z8u9yq3zrhwmua3vrkmjpqqesp0qncy';
+export const XSWAP_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgqfjxdkeeryz7w7ftjvau9h650079ku0nyqqesr7geul';
 
 // for set-settings
-export const XRAFFLE_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgqtpk48r66tz3t5zshytm2cjeqzvjha5tqqqesk6rgg6';
+export const XRAFFLE_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgqkrrqzfs27wsupnjl2q3tmkvhkx4av35yqqes30fcc7';
 export const TREASURY_ADDRESS = 'erd15936k9pw34xyzmcaupyn7lpr7f6p20q50h4wlgemxg7h9zasdfysmhg50z';
 export const RAFFLE_SC_FEE = 5_00; // 5%
 export const TREASURY_FEE = 5_00; // 5%
