84e91c751815fd99bd24dae160d9c24d3a35a842
icegriffinguru <icegriffin1122@gmail.com>
2022-12-13 17:18:43 -0500

    [Ice] x-swap fix collectIncentiveToken 2

--- x-swap/src/admin.rs
+++ x-swap/src/admin.rs
@@ -23,17 +23,12 @@ pub trait AdminModule:
     #[only_owner]
     #[endpoint(collectIncentiveToken)]
     fn collect_incentive_token(&self, opt_collect_amount: OptionalValue<BigUint>) {
-        let balance = self.blockchain().get_sc_balance(&EgldOrEsdtTokenIdentifier::esdt(self.incentive_token_id().get()), 0u64);
         let collect_amount = match opt_collect_amount {
-            OptionalValue::Some(v) => core::cmp::min(v, balance),
-            OptionalValue::None => balance,
+            OptionalValue::Some(v) => core::cmp::min(v, self.incentive_token_amount().get()),
+            OptionalValue::None => self.incentive_token_amount().get(),
         };
 
-        if collect_amount == self.incentive_token_amount().get() {  // collect all
-            self.incentive_token_amount().clear();
-        } else {
-            self.incentive_token_amount().update(|v| *v -= &collect_amount);
-        }
+        self.incentive_token_amount().update(|v| *v -= &collect_amount);
         
         self.send().direct_esdt(
             &self.blockchain().get_caller(),
