9f3116f346a68a03b86f7c683cd5f3aa90ee9619
icegriffinguru <icegriffin1122@gmail.com>
2022-12-16 22:16:03 -0500

    [Ice] x-swap: add tokens

---
+++ x-swap-test/src/3-1-add-a-tokens.ts
@@ -0,0 +1,80 @@
+import * as fs from 'fs';
+import { sendTransactions, timedOutBatchTransactionsStates } from "@elrondnetwork/dapp-core";
+import {
+	Account,
+	Address,
+	AddressValue,
+	ChainID,
+	ContractFunction,
+	GasLimit,
+	I8Value,
+	ProxyProvider,
+	SmartContract,
+	StringValue,
+	AbiRegistry,
+	SmartContractAbi,
+	Egld,
+	Balance,
+	BigUIntValue,
+	BytesValue,
+	ArgSerializer,
+	TransactionPayload,
+	Transaction,
+	TypedValue,
+	U64Value,
+	U32Value,
+	createListOfTokenIdentifiers,
+} from "@elrondnetwork/erdjs/out";
+import {
+	EXPLORER_URL,
+	XRF_TOKEN_ID,
+	COMMON_GAS_LIMIT,
+	XSWAP_SC_ADDRESS,
+	XRAFFLE_SC_ADDRESS,
+	TREASURY_ADDRESS,
+	RAFFLE_SC_FEE,
+	TREASURY_FEE,
+	WEGLD_TOKEN_ID,
+	EGLD_BASE_AMOUNT_FOR_INCENTIVE,
+	INCENTIVE_BASE_AMOUNT,
+	USDC_TOKEN_ID,
+} from "./config";
+
+import {
+	account,
+	provider,
+	signer,
+} from './provider';
+import BigNumber from 'bignumber.js';
+import {
+	sleep,
+	convertBigNumberToDate,
+	convertWeiToEsdt,
+} from './util';
+
+async function setSettings() {
+	const args: TypedValue[] = [
+		new StringValue(XRF_TOKEN_ID),	// new tokens
+	];
+
+	const { argumentsString } = new ArgSerializer().valuesToString(args);
+	const dataString = `addATokens@${argumentsString}`;
+	const data = new TransactionPayload(dataString);
+
+	const tx = new Transaction({
+			nonce: account.getNonceThenIncrement(),
+			receiver: new Address(XSWAP_SC_ADDRESS),
+			value: Balance.Zero(),
+			data: data,
+			gasLimit: new GasLimit(COMMON_GAS_LIMIT),
+	});
+
+	await signer.sign(tx);
+	const txHash = await tx.send(provider);
+	console.log(`${EXPLORER_URL}${txHash.toString()}`);
+}
+
+(async function() {
+	await account.sync(provider);
+	await setSettings();
+})();
---
+++ x-swap-test/src/3-4-get-b-tokens.ts
@@ -0,0 +1,75 @@
+import * as fs from 'fs';
+import { sendTransactions, timedOutBatchTransactionsStates } from "@elrondnetwork/dapp-core";
+import {
+	Account,
+	Address,
+	AddressValue,
+	ChainID,
+	ContractFunction,
+	GasLimit,
+	I8Value,
+	ProxyProvider,
+	SmartContract,
+	StringValue,
+	AbiRegistry,
+	SmartContractAbi,
+	Egld,
+	Balance,
+	BigUIntValue,
+	BytesValue,
+	ArgSerializer,
+	TransactionPayload,
+	Transaction,
+	TypedValue,
+	U64Value,
+	U32Value,
+	createListOfTokenIdentifiers,
+	OptionalValue,
+} from "@elrondnetwork/erdjs/out";
+import {
+	EXPLORER_URL,
+	XRF_TOKEN_ID,
+	COMMON_GAS_LIMIT,
+	XSWAP_SC_ADDRESS,
+	XRAFFLE_SC_ADDRESS,
+	TREASURY_ADDRESS,
+	RAFFLE_SC_FEE,
+	TREASURY_FEE,
+	WEGLD_TOKEN_ID,
+	EGLD_BASE_AMOUNT_FOR_INCENTIVE,
+	INCENTIVE_BASE_AMOUNT,
+	USDC_TOKEN_ID,
+	USDC_TOKEN_DECIMALS,
+	WEGLD_TOKEN_DECIMALS,
+} from "./config";
+
+import {
+	account,
+	provider,
+	signer,
+	getXSwapContractInteractor,
+} from './provider';
+import BigNumber from 'bignumber.js';
+import {
+	sleep,
+	convertBigNumberToDate,
+	convertWeiToEsdt,
+	convertEsdtToWei,
+} from './util';
+import { ValidatorPublicKey } from '@elrondnetwork/erdjs-walletcore/out';
+
+async function main() {
+	const contractInteractor = await getXSwapContractInteractor();
+	let interaction = contractInteractor.contract.methods.getBTokens();
+	let res = await contractInteractor.controller.query(interaction);
+
+	if (!res || !res.returnCode.isSuccess()) return;
+	const values = res.firstValue.valueOf();
+	const tokens = values.map(value => value.toString());
+	console.log('B Tokens: ', tokens);
+}
+
+(async function() {
+	await account.sync(provider);
+	await main();
+})();
