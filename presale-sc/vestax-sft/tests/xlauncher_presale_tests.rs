cf60919e1e46e09670d9bef6e8a88271171d8c02
Bogdan <bogdan.oloeriu@gmail.com>
2023-02-05 09:15:36 +0100

    using bigUint does not work

--- presale-sc/vestax-sft/tests/xlauncher_presale_tests.rs
+++ presale-sc/vestax-sft/tests/xlauncher_presale_tests.rs
@@ -1,4 +1,8 @@
 use multiversx_sc_scenario::*;
+use multiversx_sc_scenario::{scenario_model::*};
+
+multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
@@ -19,4 +23,33 @@ fn buy_ok_scen_01() {
 #[test]
 fn test_pricing_for_presale_round_2() {
     multiversx_sc_scenario::run_rs("scenarios/08-test-pricing-for-presale-round-2.scen.json", world());
+}
+
+#[test]
+fn test_02_fund_sft_raw() {
+    let mut world = world();
+    let ic = world.interpreter_context();
+    let owner_address = "address:owner";
+    let mut xlauncher_contract = ContractInfo::<xlauncher_presale::Proxy<DebugApi>>::new("sc:xlauncher-presale");
+
+    let token_id = "str:BCOIN-a00000";
+    let initial_price = BigUint::from(10_000_000_000_000_000_000_000);
+    let min_amount = BigUint::from(10_000_000_000_000_000_000_000);
+    let max_amount_val = BigUint::from(10_000_000_000_000_000_000_000);
+    let max_balance_val = BigUint::from(10_000_000_000_000_000_000_000);
+
+    world
+        .set_state_step(
+            SetStateStep::new()
+                .put_account("address:owner", Account::new().nonce(0))
+                .new_address("address:owner", 0, "sc:xlauncher-presale"),
+        )
+        .sc_deploy_step(
+            ScDeployStep::new()
+                .from(owner_address)
+                .contract_code("file:output/xlauncher-presale.wasm", &ic)
+                .call(xlauncher_contract.init(token_id, initial_price, min_amount, max_amount_val, max_balance_val))
+                .gas_limit("5,000,000")
+                .expect(TxExpect::ok().no_result()),
+        );
 }
